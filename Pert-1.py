02769e0db2d668933d4aaab2f29c13292c1bf8e8
Andi Irmayanti <irmayantiandi@gmail.com>
2021-10-25 15:24:42 +0700

    materi training py

---
+++ Pert-1.py
@@ -0,0 +1,26 @@
+# title
+print ("Selamat Pagi Dunia Tipu-tipu")
+
+# variables
+a = 5
+b = "Selamat Pagi Dunia Tipu-tipu"
+c = 10
+profile= "Hi semua namaku Irmachi"
+
+
+print (a*c)
+print (b)
+
+# check jenis data
+print (type(a))
+print (type (profile))
+
+# rubah jenis data (casting)
+print (str(a))
+
+n = 2 * 1.1
+
+print (type(n))
+print (n)
+
+print (round (n))
