801312009cc8606db4017613fed6431dfda91f05
Andi Irmayanti <irmayantiandi@gmail.com>
2021-11-01 16:04:38 +0700

    Pert-5

---
+++ Pert-5.py
@@ -0,0 +1,43 @@
+#########
+#MODULE
+#########
+
+# import mymodule
+
+# mymodule.greeting ("Irma")
+
+# import math
+# import datetime
+
+# # x=math.sqrt(64)
+# # print(x)
+
+# x=math.pi
+# print(x)
+
+# print(datetime.datetime.now())
+
+#########
+# FILE HANDLING
+#########
+
+# f = open("file.txt", "a")
+# f.write("Added text.")
+# f.close()
+
+# f = open("file.txt", "a")
+# f.write("Hello!\n")
+# f.close()
+
+# f = open("file.txt", "r")
+# print(f.read(5))
+
+# f = open("file.txt", "r")
+# print(f.readline())
+
+# import os
+# os.remove("file.txt")
+
+import matematika
+print("PI:", matematika.pi)
+print(matematika.luas_persegi(sisi=12))
---
+++ mymodule.py
@@ -0,0 +1,8 @@
+def greeting(name):
+    print("Hello, " + name)
+
+def goodbye(name):
+    print("Goodbye " + name + ", see you later!")
+
+def check (name):
+    print (name)
